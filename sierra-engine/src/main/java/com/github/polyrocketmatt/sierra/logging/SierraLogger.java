c6c6bf32dc648264efc9968d278163a770cc30b4
Matthias Kovacic <rocketpencil.studios@gmail.com>
2023-02-06 17:26:48 +0100

    Make SierraLogger threaded to not interfere with performance

--- sierra-engine/src/main/java/com/github/polyrocketmatt/sierra/logging/SierraLogger.java
+++ sierra-engine/src/main/java/com/github/polyrocketmatt/sierra/logging/SierraLogger.java
@@ -5,23 +5,46 @@ import java.io.File;
 import java.io.FileWriter;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.Semaphore;
 
-public class SierraLogger {
+public class SierraLogger extends Thread {
 
     private static SierraLogger logger;
 
     private final SimpleDateFormat format;
     private final File logFile;
     private final BufferedWriter writer;
+    private final Semaphore semaphore;
+    private final Timer timer;
 
     private SierraLogger(File logFile, String format) {
         try {
             this.format = new SimpleDateFormat(format);
             this.logFile = logFile;
             this.writer = new BufferedWriter(new FileWriter(logFile, true));
+            this.semaphore = new Semaphore(1);
+            this.timer = new Timer();
         } catch (Exception ex) {
             throw new RuntimeException("Unable to create logger for file " + logFile.getName(), ex);
         }
+    }
+
+    @Override
+    public void run() {
+        TimerTask flushTask = new TimerTask() {
+            @Override
+            public void run() {
+                try {
+                    flush();
+                } catch (Exception ex) {
+                    throw new RuntimeException("Unable to flush log file " + logFile.getName(), ex);
+                }
+            }
+        };
+
+        timer.schedule(flushTask, 0, 5000);
     }
 
     private void writeLog(String message) {
@@ -57,9 +80,31 @@ public class SierraLogger {
         this.writeLog("%s [%s] [%s] %s".formatted(format.format(new Date()), LogLevel.WARNING, type.name(), message));
     }
 
+    private void waitAndWrite(LogLevel level, String message) throws InterruptedException {
+        semaphore.acquire();
+        switch (level) {
+            case INFO ->        this.logInform(message);
+            case WARNING ->     this.logWarn(message);
+            case ERROR ->       this.logError(message);
+        }
+        semaphore.release();
+    }
+
+    private void waitAndWrite(LogLevel level, String message, LogType type) throws InterruptedException {
+        semaphore.acquire();
+        switch (level) {
+            case INFO ->        this.logInform(message, type);
+            case WARNING ->     this.logWarn(message, type);
+            case ERROR ->       this.logError(message, type);
+        }
+        semaphore.release();
+    }
+
     private void flush() {
         try {
+            semaphore.acquire();
             writer.flush();
+            semaphore.release();
         } catch (Exception ex) {
             throw new RuntimeException("Unable to flush log file " + logFile.getName(), ex);
         }
@@ -67,7 +112,10 @@ public class SierraLogger {
 
     private void close() {
         try {
+            semaphore.acquire();
             writer.close();
+            timer.cancel();
+            semaphore.release();
         } catch (Exception ex) {
             throw new RuntimeException("Unable to close log file " + logFile.getName(), ex);
         }
@@ -92,30 +140,47 @@ public class SierraLogger {
             logger.close();
 
         logger = new SierraLogger(logFile, format);
+        logger.start();
     }
 
-    public void inform(String message) {
-        this.logInform(message);
+    private static void write(LogLevel level, String message) {
+        try {
+            logger.waitAndWrite(level, message);
+        } catch (InterruptedException ex) {
+            throw new RuntimeException("Unable to write to log file " + logger.logFile.getName(), ex);
+        }
     }
 
-    public void inform(String message, LogType type) {
-        this.logInform(message, type);
+    private static void write(LogLevel level, String message, LogType type) {
+        try {
+            logger.waitAndWrite(level, message, type);
+        } catch (InterruptedException ex) {
+            throw new RuntimeException("Unable to write to log file " + logger.logFile.getName(), ex);
+        }
     }
 
-    public void warn(String message) {
-        this.logWarn(message);
+    public static void inform(String message) {
+        write(LogLevel.INFO, message);
     }
 
-    public void warn(String message, LogType type) {
-        this.logWarn(message, type);
+    public static void inform(String message, LogType type) {
+        write(LogLevel.INFO, message, type);
     }
 
-    public void error(String message) {
-        this.logError(message);
+    public static void warn(String message) {
+        write(LogLevel.WARNING, message);
     }
 
-    public void error(String message, LogType type) {
-        this.logError(message, type);
+    public static void warn(String message, LogType type) {
+        write(LogLevel.WARNING, message, type);
+    }
+
+    public static void error(String message) {
+        write(LogLevel.ERROR, message);
+    }
+
+    public static void error(String message, LogType type) {
+        write(LogLevel.ERROR, message, type);
     }
 
 }
