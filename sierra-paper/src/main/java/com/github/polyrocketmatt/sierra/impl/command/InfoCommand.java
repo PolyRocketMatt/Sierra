8dd1232d18ce9e742dc8474044d5a66b805480a2
Matthias Kovacic <rocketpencil.studios@gmail.com>
2023-02-06 20:41:27 +0100

    Improve info command

--- sierra-paper/src/main/java/com/github/polyrocketmatt/sierra/impl/command/InfoCommand.java
+++ sierra-paper/src/main/java/com/github/polyrocketmatt/sierra/impl/command/InfoCommand.java
@@ -1,7 +1,11 @@
 package com.github.polyrocketmatt.sierra.impl.command;
 
+import com.github.polyrocketmatt.delegate.api.command.feedback.FeedbackType;
+import com.github.polyrocketmatt.delegate.core.permission.PermissionTiers;
 import com.github.polyrocketmatt.delegate.impl.Delegate;
 import com.github.polyrocketmatt.sierra.impl.Sierra;
+
+import java.util.Objects;
 
 import static com.github.polyrocketmatt.sierra.engine.utils.StringUtils.PAPER_PREFIX;
 
@@ -13,6 +17,11 @@ public class InfoCommand extends SierraCommand {
                 .withConsumerAction("info", (commander, args) -> {
                     commander.sendMessage(PAPER_PREFIX + "&7Sierra Information");
                     commander.sendMessage(PAPER_PREFIX + "&7Version: &6%s".formatted(Sierra.version()));
+                })
+                .withPermission(PermissionTiers.OPERATOR.getTier())
+                .onExcept((commander, type, args) -> {
+                    if (Objects.requireNonNull(type) == FeedbackType.UNAUTHORIZED)
+                        commander.sendMessage("%s &cOops, you do not have permission to use this command!".formatted(PAPER_PREFIX));
                 })
                 .build();
     }
